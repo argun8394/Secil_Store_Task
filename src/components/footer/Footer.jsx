8b8229e99730d05d673b99756051eb7c1710ff92
argun8394 <argundoganali@gmail.com>
2024-06-07 11:54:48 +0300

    create_[footer and navbar]

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className=" flex justify-center items-center h-32 bg-slate-600">
+            <h2>Footer</h2>
+        </div>
     )
 }
 
