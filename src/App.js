72586c6136a6a6661203fae11e4b2441b8c62ffa
argun8394 <argundoganali@gmail.com>
2024-06-07 11:56:50 +0300

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import Layout from "./layout";
+import { BrowserRouter } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-700">
-        Hello world!
-      </h1>
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Layout />
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Products from "../components/products/Products";
+
+const Home = () => {
+    return (
+        <div className="bg-white">
+            <Products />
+        </div>
+    );
+};
+
+export default Home;
