f176eb5cb87ed3cff008c8a4638bcd3cba26191c
argun8394 <argundoganali@gmail.com>
2024-06-07 11:56:06 +0300

    add_store

---
+++ src/features/productsSlice.jsx
@@ -0,0 +1,49 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    products: [],
+    loading: false,
+    error: false,
+};
+
+export const getProducts = createAsyncThunk(
+    "getProductsFunc",
+
+    async () => {
+        const url = `https://fakestoreapi.com/products`;
+
+        const { data } = await axios(url);
+        return data;
+    }
+);
+
+const productsSlice = createSlice({
+    name: "products",
+    initialState,
+    reducers: {
+        clearProducts: (state) => {
+            state.products = [];
+        },
+    },
+
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(getProducts.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(getProducts.fulfilled, (state, action) => {
+                state.products = action.payload;
+                state.loading = false;
+            })
+            .addCase(getProducts.rejected, (state) => {
+                state.error = true;
+                state.loading = false;
+            });
+    },
+});
+
+export const { clearProducts } = productsSlice.actions;
+
+export default productsSlice.reducer;
