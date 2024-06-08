6a1d1d11e15e11e3340c50b5ccc7d419fe013cbe
argun8394 <argundoganali@gmail.com>
2024-06-08 11:30:15 +0300

    create_socialMedia

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import SocialMedia from "../socialMedia/SocialMedia";
+
+const links = [
+    { title: "Home", path: "/" },
+    { title: "About", path: "/about" },
+];
 
 const Footer = () => {
     return (
-        <div className=" flex justify-center items-center h-32 bg-slate-600">
-            <h2>Footer</h2>
+        <div className="w-full bg-[#120a2e] text-gray-500">
+            <div className="navBarPadding containerDiv flex flex-col justify-around items-center  mx-auto h-[200px] px-4 w-full ">
+                <div className="flex flex-col min-[560px]:flex-row   justify-between w-full gap-5 my-2">
+                    <div className="flex justify-center font-bold text-[26px]">Seçil Store</div>
+                    <div className="flex justify-center items-center gap-4">
+                        {links.map((link) => (
+                            <Link
+                                to={link.path}
+                                key={link.title}
+                                className="font-[500] text-[20px]"
+                            >
+                                {link.title}
+                            </Link>
+                        ))}
+                    </div>
+                    <div className="flex justify-center items-center ">
+                        <SocialMedia />
+                    </div>
+                </div>
+                <div >
+                    <p> © Seçil Store | All Rights Reserved</p>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Footer
+export default Footer;
