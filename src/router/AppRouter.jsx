30c67f061c59cfa5e31ccc46840c4cebdc3e9ba0
argun8394 <argundoganali@gmail.com>
2024-06-08 11:29:51 +0300

    update_router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home"
+import About from '../pages/About';
 import ProductDetail from '../pages/ProductDetail';
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 
 const AppRouter = () => {
     return (
-        <>
+        <Router>
             <Navbar />
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route >
-                    <Route path="/details/:id" element={<ProductDetail />} />
-                </Route>
+                <Route path="/about" element={<About />} />
+                <Route path="/details/:id" element={<ProductDetail />} />
             </Routes>
             <Footer />
-        </>
+        </Router>
     )
 }
 
